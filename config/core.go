4a08e44a9da4ee28e11952428e76fd89a5d17c9f
kiquetal <kiquetal@gmail.com>
2024-01-21 12:52:21 -0300

    Passing variable to defaults

--- config/core.go
+++ config/core.go
@@ -1,6 +1,11 @@
 package config
 
-import "os"
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"os"
+)
 
 type Configuration struct {
 	Port            string `json:"port"`
@@ -17,6 +22,29 @@ func (c *Configuration) LoadFromEnv() {
 	if port := os.Getenv("PORT"); port != "" {
 		c.Port = port
 	}
+}
+
+func (c *Configuration) LoadFromJSON(path string) error {
+	log.Printf("Loading configuration from %s", path)
+	b, err := os.ReadFile(path)
+	if err != nil {
+		log.Printf("Unable to read configuration file %s,%s", path, err)
+		return errors.New("unable to read configuration file")
+
+	}
+	if err := json.Unmarshal(b, c); err != nil {
+		log.Printf("Unable to parse configuration file %s,%s", path, err)
+		return errors.New("unable to parse configuration file")
+	}
+	if c.Port == "" {
+		log.Printf("Empty port, reverting to default")
+		c.Port = defaultConfiguration.Port
+	}
+	if c.DefaultLanguage == "" {
+		log.Printf("Empty default language, reverting to default")
+		c.DefaultLanguage = defaultConfiguration.DefaultLanguage
+	}
+	return nil
 }
 
 var defaultConfiguration = Configuration{
